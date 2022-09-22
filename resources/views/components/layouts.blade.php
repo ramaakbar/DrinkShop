5f58ae30aaa1a805b2c15971a4483b626fcb17e0
rama <akbar.6b@gmail.com>
2022-09-22 22:37:47 +0700

    Update link css and script

--- resources/views/components/layouts.blade.php
+++ resources/views/components/layouts.blade.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    <link rel="stylesheet" href="http://drinkshop-production.up.railway.app/css/app.css">
 
     <title>Drink Shop</title>
-    <script src="{{ asset('js/mapInput.js') }}"></script>
+    {{-- <script src="{{ asset('js/mapInput.js') }}"></script> --}}
+    <script src="http://drinkshop-production.up.railway.app/js/mapInput.js"></script>
 </head>
 
 <body class="font-body">
@@ -27,11 +29,15 @@
             })
         }
     </script>
-        
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    
-    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY')}}&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC" async defer></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC"
+        async defer></script>
 
 
     <script src="{{ asset('js/script.js') }}"></script>
